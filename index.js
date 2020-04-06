c811e4c2dd99957e9b10cff3b83c87b3d1549546
ayushbihani <bihani37@gmail.com>
2020-04-06 00:00:26 -0700

    Adding control origin for frontend

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json({extended:false}));
 app.use(function(req, res, next){
-    res.header("Access-Control-Allow-Origin", "https://bihanihw8.azurewebsites.net/");
+    res.header("Access-Control-Allow-Origin", "https://bihanihw8.azurewebsites.net");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
